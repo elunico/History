71a4812a6a9162677d2ce842cc2baee2162284e3
Thomas Povinelli <thomaspovinelli@sabro.fios-router.home>
2017-06-20 00:06:49 -0400

    Packaged classes; added execute action method in history

--- src/tom/history/Action.java
+++ src/tom/history/Action.java
@@ -1,11 +1,13 @@
+package tom.history;
+
 /**
  * This interface is used by {@link History} to implement undo and redo
  * This class functions <i>similar</i> to a functional interface except that
  * you must define 3 methods to define the behavior of execution (this method
  * is like the run method of a {@link Runnable} or the handle method of an
  * {@link java.beans.EventHandler}. It is the first method called and can even
- * be called in the History class using {@link History#registerActionAndExecute(Action)}
- * method. It defines the way to execute the Action for the first time
+ * be called in the tom.history.History class using {@link History#registerActionAndExecute(Action)}
+ * method. It defines the way to execute the tom.history.Action for the first time
  * <p>
  * The method {@link #undo()} defines how to undo the action. This often requires
  * adding fields to the implementing class to contain the data that needs to be
@@ -14,7 +16,7 @@
  * While this may seem similar to (or even exactly the same as) the {@link #execute()}
  * method, it may also be different. This method is called by {@link History#redo()}.
  * <p>
- * Take for example an Action which chooses a random number and sets the Text of
+ * Take for example an tom.history.Action which chooses a random number and sets the Text of
  * a label to that random number. Undoing this action would require you to have
  * stored the previous state of the Label before executing in a field in the class.
  * Likewise, redoing this would not involve choosing a random number but rather
@@ -28,7 +30,7 @@
 public interface Action {
 
     /**
-     * Called to execute the action. It is called when the Action is to proceed
+     * Called to execute the action. It is called when the tom.history.Action is to proceed
      * or take place for the first time. Also called by {@link History#registerActionAndExecute(Action)}
      * While this method need not check to ensure, the execute method should only
      * be called in code once and then {@link #undo()} and {@link #redo()} should
